be02b2984adc432bd99beef80b632a68980522d1
Carrie Mathieu <carrieburks@gmail.com>
2021-04-09 19:31:23 -0400

    refactor input

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -2,19 +2,29 @@ import React from 'react'
 
 class SearchBar extends React.Component {
     // called anytime someone inputs text
-    onInputChange(event) {
-        // contains whatever is typed in
-        console.log(event.target.value)
-    }
+    // onInputChange(event) {
+    //     // contains whatever is typed in
+    //     console.log(event.target.value)
+    // }
 
-    // do not add () to onInputChange callback
+    state = { term: '' }
+    // **controlled element**
+    // user types in input
+    // callback gets invoked
+    // call setState w/ new value
+    // component re-renders
+    // input told it's value (coming from state)
     render() {
         return (
             <div className="ui segment">
                 <form className="ui form">
                     <div className="field">
                         <label>Image Search</label>
-                        <input type="text" onChange={this.onInputChange}></input>
+                        <input 
+                            type="text" 
+                            value={this.state.term}
+                            onChange={(e)=> this.setState({ term: e.target.value })}
+                        />
                     </div>
                 </form>
             </div>

28798cadab4d09e744ec2122e20b4a30dc4d14b0
Carrie Mathieu <carrieburks@gmail.com>
2021-04-14 22:13:41 -0400

    modified CSS

--- src/components/ImageCard.js
+++ src/components/ImageCard.js
@@ -4,19 +4,24 @@ class ImageCard extends React.Component {
     constructor(props) {
         super(props)
 
+        this.state = { spans: 0 }
         // allows us to get the height of each element / current property
         this.imageRef = React.createRef()
     }
 
     // let componenet render, then reach into DOM to get image height
-    componenetDidMount() {
+    componentDidMount() {
         // have to add event listener bc otherwise, we'll get 'height' before image is loaded into DOM (0)
-        this.imageRef.current.addEventListener('load', this.setSpans)
+        this.imageRef.current.addEventListener('load', this.setSpans);
     }
 
     // arrow func so we still have acces to "this"
     setSpans = () => {
+        const height = this.imageRef.current.clientHeight;
 
+        const spans = Math.ceil(height / 10)
+
+        this.setState({ spans })
     }
 
     render() {
@@ -25,7 +30,7 @@ class ImageCard extends React.Component {
 
         // each image is nested under "urls" / "regular" from API, 
         return(
-            <div>
+            <div style={{ gridRowEnd: `span ${this.state.spans}` }}>
                 <img ref={this.imageRef} alt={description} src={urls.regular}/>
             </div>
         )
